70c65f158991a8a58d568048c850f31f433fd6fd
zoli87webdev <zoli87webdev@gmail.com>
2025-07-08 09:11:43 +0200

    vendor:publish=errors, PostController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
                             <a href="https://cloud.laravel.com" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                                Deploy now
+                                hello
                             </a>
                         </li>
                     </ul>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'name'=> 'Zsuzsi',
+    ]);
+});
+
+Route::get('/pass-array' , function(){
+    $tasks = [
+        'Learn Laravel',
+        'Build a web app',
+        'Deploy to production',
+    ];
+    return view('tasks', [
+        'tasks' => $tasks,
+    ]);
+});
+
+Route::get('/request-test', function () {
+    return view('request-input', [
+        'title' => request('title'),
+    ]);
+});
+
+// Route::get('/posts/{post}', function ($post){
+//     return view('posts', [
+//         'post' => $post,
+//     ]);
+// });
+
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'first-post' => 'My first post',
+        'second-post' => 'My second post',
+        'third-post' => 'My third post',
+    ];
+
+    if ( ! array_key_exists($post, $posts)) { abort(404); }
+
+    
+    return view('posts', [
+        'post' => $posts[$post] ,
+    ]);
+});
+
+
